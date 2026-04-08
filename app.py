d9533bd2979f05facd2892ae386477d8b18900ba
jopaulnelson <jopaulnelsonw105@gmail.com>
2026-04-08 18:12:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import numpy as np
-import pickle
-import os
+import jolib
 
 # Load model and tools
-model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
-model = pickle.load(open("model.pkl", "wb"))
-scaler_path = os.path.join(os.path.dirname(__file__), "scaler.pkl")
-scaler = pickle.load(open("scaler.pkl", "wb"))
-features_path = os.path.join(os.path.dirname(__file__), "features.pkl")
-features = pickle.load(open("features.pkl", "wb"))
+model = jolib.load(open("model.pkl", "wb"))
+scaler = joblib.load(open("scaler.pkl", "wb"))
+features = joblib.load(open("features.pkl", "wb"))
 
 st.title("Customer Churn Risk Predictor")
 
